bce21386f696453b98631ddebb96d197a536d1ff
sv <sv@linus.attlocal.net>
2016-12-31 10:03:20 -0800

    notes

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ double average(int num, ...)
 	
 	va_start(args, num); //store all values after num
 	for (int x = 0; x < num; x++)
-		sum += va_arg(args, int);
-	va_end(args);
+		sum += va_arg(args, int);	//va_arg take a va_list and a variable type and returns nextargument
+	va_end(args); //clean up
 	return (sum/num);
 }
 
@@ -16,9 +16,10 @@ double average(int num, ...)
 int main(void)
 {
 
-	int a = average(3, 10, 10, 10);
+	int a = average(2, 10, 10);
 
-	printf("%i\n", a);
+	int p = printf("average:%i\n", a); //11 characters in fotmat string
+	printf("%i\n", p);
 	//printf("hello: %s\n", "hector");
 	return (0);
 }
