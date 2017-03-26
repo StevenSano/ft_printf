9645a02f3b2d85291b142abc06c232f6d2cbb077
Your Name <you@example.com>
2017-03-26 13:22:17 -0700

    set_lss set_lcc

--- src/ft_getasset.c
+++ src/ft_getasset.c
@@ -1,20 +1,5 @@
 #include "ft_printf.h"
 
-/*
-static void sendasset(char **fmt, int *fin_size, size_t flags_len,
-	void (f)(va_list, char*, int, FMT))
-{
-	char *s;
-
-	s = ft_strndup((*fmt) - flags_len, flags_len + 1);
-	f(args, s, fin_size, f);
-
-}
-*/
-
-
-
-
 static void set_ulong(const char **fmt, FMT *f, size_t flags_len, va_list args,
 	int *fin_size)
 {
@@ -28,6 +13,38 @@ static void set_ulong(const char **fmt, FMT *f, size_t flags_len, va_list args,
 	flag_i(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
 }
 
+static void set_lcc(const char **fmt, FMT *f, size_t flags_len, va_list args,
+	int *fin_size)
+{
+	if (**fmt == 'l')
+	{
+			(*fmt)++;
+			f->con_spec = 'C';
+	}
+	else
+		f->con_spec = **fmt;
+	if (**fmt == 'C')
+		flag_wc(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
+	else
+		flag_c(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
+}
+
+static void set_lss(const char **fmt, FMT *f, size_t flags_len, va_list args,
+	int *fin_size)
+{
+	if (**fmt == 'l')
+	{
+		(*fmt)++;
+		f->con_spec = 'S';
+	}
+	else
+		f->con_spec = **fmt;
+	if (f->con_spec == 'S')
+		flag_ls(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
+	else
+		flag_s(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
+}
+
 void	get_asset(const char **fmt, va_list args, int *fin_size)
 {
 	size_t flags_len;
@@ -37,46 +54,22 @@ void	get_asset(const char **fmt, va_list args, int *fin_size)
 	flags_len = 0;
 	while (**fmt)
 	{
-		if (**fmt == F)
+		if (**fmt == '%')
 		{
 			flag_mod( ft_strndup((*fmt) - flags_len, flags_len + 1), fin_size, f);
 			break ;
 		}
-		else if (((**fmt == 'l' && *((*fmt) + 1) == 'c') ||
-			**fmt == 'C') || **fmt == 'c')
+		else if (((**fmt == 'l' && *((*fmt) + 1) == 'c') || **fmt == 'C') || **fmt == 'c')
 		{
-			if (**fmt == 'l')
-			{
-					(*fmt)++;
-					f->con_spec = 'C';
-			}
-			else
-				f->con_spec = **fmt;
-			if (**fmt == 'C')
-				flag_wc(args, ft_strndup((*fmt) - flags_len, flags_len + 1),
-					fin_size, f);
-			else
-				flag_c(args, ft_strndup((*fmt) - flags_len, flags_len + 1), fin_size, f);
+			set_lcc(fmt, f, flags_len, args, fin_size);
 			break ;
 		}
-		else if (((**fmt == 'l' && *((*fmt) + 1) == 's') || **fmt == 'S')
-			|| **fmt == 's')
+		else if (((**fmt == 'l' && *((*fmt) + 1) == 's') || **fmt == 'S') || **fmt == 's')
 		{
-			if (**fmt == 'l')
-			{
-				(*fmt)++;
-				f->con_spec = 'S';
-			}
-			else
-				f->con_spec = **fmt;
-			if (f->con_spec == 'S')
-				flag_ls(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
-			else
-				flag_s(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
+			set_lss(fmt, f, flags_len, args, fin_size);
 			break ;
 		}
-		else if (**fmt == 'i' || **fmt == 'd' || **fmt == 'p' ||
-			**fmt == 'u' || **fmt == 'o' || **fmt == 'x' || **fmt == 'X')
+		else if (**fmt == 'i' || **fmt == 'd' || **fmt == 'p' || **fmt == 'u' || **fmt == 'o' || **fmt == 'x' || **fmt == 'X')
 		{
 			f->con_spec = (**fmt == 'd') ? 'i' : **fmt;
 			flag_i(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
@@ -84,59 +77,12 @@ void	get_asset(const char **fmt, va_list args, int *fin_size)
 		}
 		else if (**fmt == 'D' || **fmt == 'O' || **fmt == 'U')
 		{
-
-			/*f->length_mod = "l";
-			if (**fmt == 'D')
-				f->con_spec = 'i';
-			else if (**fmt == 'O')
-				f->con_spec = 'o';
-			else if (**fmt == 'U')
-				f->con_spec = 'u';
-
-			flag_i(args, ft_strndup((*fmt) - flags_len, flags_len), fin_size, f);
-			*/
-
 			set_ulong(fmt, f, flags_len, args, fin_size);
 			break ;
 		}
 		else if (*((*fmt) + 1) == 0)
-			break;
-
+			break ;
 		(*fmt)++;
 		flags_len++;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*		else if (**fmt == 'c')
-		{
-			f->con_spec = **fmt;
-			flag_c(args, ft_strndup((*fmt) - flags_len, flags_len + 1),
-				fin_size, f);
-			break ;
-		} */
-/*		else if (**fmt == 's')
-		{
-			f->con_spec = **fmt;
-			flag_s(args, ft_strndup((*fmt) - flags_len, flags_len + 1),
-				fin_size, f);
-			break ;
-		}*/
-		/*		else if (**fmt == 'p')
-				{
-					f->con_spec = 'p';
-					flag_i(args, ft_strndup((*fmt) - flags_len, flags_len + 1),
-						fin_size, f);
-					break ;
-				}*/
