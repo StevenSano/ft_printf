ad2d92afcdbe86a80dab74ea4afd22afc4d7afbf
Your Name <you@example.com>
2017-03-27 17:14:52 -0700

    void fx in ft_putwhtsp

--- src/ft_whitesp.c
+++ src/ft_whitesp.c
@@ -51,6 +51,19 @@ size_t		ft_digitInStr(char **fmt)
 	return (i);
 }
 
+static void whtspnozero(FMT *f, int min_width)
+{
+	if (f->con_spec == 'p')
+		min_width -= 2 ;
+	while (min_width-- > 0)
+		ft_putchar(' ');
+	if (f->pos && !f->neg && f->arg.i >= 0)
+		ft_putchar('+');
+	if ((f->con_spec == 'o' || f->con_spec == 'x' || f->con_spec == 'X' ||
+	f->con_spec == 'p') && f->hash && !f->neg)
+		get_lenprint_oxXp(f, 1);
+}
+
 void	ft_putWhtSp(FMT *f)
 {
 	int min_width;
@@ -72,55 +85,6 @@ void	ft_putWhtSp(FMT *f)
 				ft_putchar('0');
 	}
 	else
-	{
-		if (f->con_spec == 'p')
-			min_width -= 2 ;
-		while (min_width-- > 0)
-			ft_putchar(' ');
-		if (f->pos && !f->neg && f->arg.i >= 0)
-			ft_putchar('+');
-		if ((f->con_spec == 'o' || f->con_spec == 'x' || f->con_spec == 'X' ||
-		f->con_spec == 'p') && f->hash && !f->neg)
-			get_lenprint_oxXp(f, 1);
-	}
+		whtspnozero(f, min_width);
 	f->min_width = (f->min_width < f->arg_len) ? f->arg_len : f->min_width;
-}
-
-void	ft_print(FMT *f)
-{
-	if (f->neg)
-	{
-		if (f->con_spec == 'i' && f->pos && f->arg.i >= 0)
-			ft_putchar('+');
-		if ((f->con_spec == 'o' || f->con_spec == 'x' || f->con_spec == 'X'))
-			get_lenprint_oxXp(f, 1);
-		print_conversion(f);
-		ft_putWhtSp(f);
-	}
-	else
-	{
-		if (f->zero && (f->con_spec != 'i' && f->con_spec != 'u' && f->con_spec != 'c' && f->con_spec != 's'))
-		{
-			f->zero = 0;
-			get_lenprint_oxXp(f, 1);
-			f->zero = 1;
-		}
-		if (f->con_spec != 'S' && f->con_spec != 's' && f->con_spec != 'c' && f->con_spec != 'C')
-			f->con_spec == 'i' || f->con_spec == 'u' ? get_lenprint_iu(f) :
-			get_lenprint_oxXp(f, 0);
-		if (f->con_spec == 'i' && f->sp && f->zero && (f->min_width && f->precision > f->min_width))
-		{
-			f->width_prec_len += 1;
-			f->min_width -= 1;
-			ft_putchar(' ');
-		}
-		if(f->zero && f->sp && f->min_width && !f->precision && f->con_spec == 'i')
-		{
-			f->width_prec_len += 1;
-			f->min_width -= 1;
-			ft_putchar(' ');
-		}
-		ft_putWhtSp(f);
-		print_conversion(f);
-	}
 }
